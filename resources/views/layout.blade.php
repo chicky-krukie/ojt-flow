7bcfd82a45a4b87a4c2865e8b055559539874c4e
chicky-krukie <erica.flores0307@gmail.com>
2023-04-10 11:35:47 -0700

    init

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,40 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+    <title>ojt flow</title>
+</head>
+<style>
+    body {
+        font-family: 'Nunito', sans-serif;
+    }
+    
+</style>
+
+<body class="col-lg-12">
+<nav class="navbar navbar-expand-lg navbar-light bg-light col-lg-12">
+  <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+    <div class="navbar-nav">
+      <a class="nav-item nav-link active" href="{{ url('/') }}">Inventory<span class="sr-only">(current)</span></a>
+      <a class="nav-item nav-link" href="{{ url('/orders') }}">Orders</a>
+      <a class="nav-item nav-link" href="{{ url('/settings') }}">Settings</a>
+    </div>
+  </div>
+</nav>
+
+
+<div>
+    @yield('content')
+</div>
+
+
+
+
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+</body>
+</html>
