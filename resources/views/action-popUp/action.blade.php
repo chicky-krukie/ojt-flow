a5d407e174c248bdbff38b4c2fa3136e6441c5a4
Ngebu <kimfrancis.villacer@tup.edu.ph>
2023-04-18 14:35:17 -0700

    symbolupdate

--- resources/views/action-popUp/action.blade.php
+++ resources/views/action-popUp/action.blade.php
@@ -3,7 +3,7 @@
     <div class="modal-dialog">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title" id="myModalLabel">Sold</h5>
+                <h5 class="modal-title" id="myModalLabel">{{ $item->name }} Sold</h5>
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <form action="{{ route('csv.update', $item->uid) }}" method="post" enctype="multipart/form-data">
@@ -20,46 +20,63 @@
                             max="10" data-row="{{ $item->uid }}">
                     </div>
 
-                    <strong>Sold Price (Peso):</strong>
+                    <strong>Sold Price:</strong>
                     <div class="mb-3 input-group">
-                        <div class="input-group-text">₱</div>
+                        <div class="input-group-text">
+                            @foreach ($settings['currency_option'] as $currency)
+                                @if ($settings['sold_price'] === $currency['id'])
+                                    {{ $currency['symbol'] }}
+                                @endif
+                            @endforeach
+                        </div>
                         <input type="text" name="sold"
                             value="{{ floatval($settings['multiplier_default']) * floatval(preg_replace('/[^-0-9\.]/', '', $csv_outputs[$index]->total)) }}"
                             class="form-control sold">
                     </div>
 
-                    <div class="mb-3">
-                        <strong>Ship price:</strong>
+                    <strong>Ship price:</strong>
+                    <div class="mb-3 input-group">
+                        <div class="input-group-text">
+                            @foreach ($settings['currency_option'] as $currency)
+                                @if ($settings['ship_price'] === $currency['id'])
+                                    {{ $currency['symbol'] }}
+                                @endif
+                            @endforeach
+                        </div>
                         <input type="text" name="ship_price" value="0" class="form-control" placeholder="">
                     </div>
 
-                    <div class="mb-3">
-                        <strong>Ship cost:</strong>
+                    <strong>Ship cost:</strong>
+                    <div class="mb-3 input-group">
+                        <div class="input-group-text">
+                            @foreach ($settings['currency_option'] as $currency)
+                                @if ($settings['ship_cost'] === $currency['id'])
+                                    {{ $currency['symbol'] }}
+                                @endif
+                            @endforeach
+                        </div>
                         <input type="text" name="ship_cost" value="0" class="form-control" placeholder="">
                     </div>
 
                     <div class="mb-3">
-
-
-                        <input type="text" name="payment_status"
-                            value="{{ $settings['payment_status'][2]['status'] }}" class="hidden d-none">
+                        <input type="text" name="payment_status" value="{{ $settings['status'][2]['status'] }}"
+                            class="hidden d-none">
                     </div>
 
                     <div class="mb-3">
                         <strong>Payment Method</strong>
                         <select name="payment_methods" class="form-control">
                             <option value="" disabled selected hidden>Enter your mode of payment</option>
-                            @foreach ($settings['payment_methods'] as $setting)
+                            @foreach ($settings['method'] as $setting)
                                 <option value="{{ $setting['method'] }}">{{ $setting['method'] }}</option>
                             @endforeach
-
-                        </select><br>
+                        </select>
                     </div>
 
                     <div class="mb-3">
                         <strong>Multiplier:</strong>
-                        <input type="text" name="multiplier" value="{{ $settings['multiplier_default'] }}"
-                            class="form-control multiplier" placeholder="">
+                        <input type="number" name="multiplier" value="{{ $settings['multiplier_default'] }}"
+                            class="form-control multiplier" min="1" max="60">
                     </div>
 
                     <strong>Multiplied Price:</strong>
@@ -70,9 +87,16 @@
                             class="form-control multiplied_price" placeholder="">
                     </div>
 
-                    <div class="mb-3">
-                        <strong>Estimated Card Cost:</strong>
-                        ₱{{ floatval($settings['multiplier_cost']) * floatval(preg_replace('/[^-0-9\.]/', '', $csv_outputs[$index]->price_each)) }}
+                    <strong>Estimated Card Cost:</strong>
+                    <div class="mb-3 input-group">
+                        <div class="input-group-text">
+                            @foreach ($settings['currency_option'] as $currency)
+                                @if ($settings['estimated_card_cost'] === $currency['id'])
+                                    {{ $currency['symbol'] }}
+                                @endif
+                            @endforeach
+                            {{ floatval($settings['multiplier_cost']) * floatval(preg_replace('/[^-0-9\.]/', '', $csv_outputs[$index]->price_each)) }}
+                        </div>
                     </div>
 
 
@@ -84,10 +108,11 @@
 
                 </div>
 
+                {{-- fotter button --}}
                 <div class="modal-footer">
                     <button type="button" class="btn btn-secondary" data-bs-dismiss="modal"><i class="fa fa-times"></i>
                         Cancel</button>
-                    <button type="submit" class="btn btn-success"><i class="fa fa-check-square-o"></i>Sold</button>
+                    <button type="submit" class="btn btn-success"><i class="fa fa-shopping-cart"></i>   Sold</button>
                 </div>
             </form>
         </div>
@@ -112,6 +137,7 @@
                     <h4 class="text-center">Are you sure you want to delete this row?</h4>
 
                 </div>
+                {{-- fotter button --}}
                 <div class="modal-footer">
                     <button type="button" class="btn btn-secondary" data-bs-dismiss="modal"><i
                             class="fa fa-times"></i> Cancel</button>
--- resources/views/settings.blade.php
+++ resources/views/settings.blade.php
@@ -1,111 +1,118 @@
 @extends('layout')
 @section('content')
-<div class="col-lg-12">
-    <h2 class="my-4">Settings</h2>
-    <hr>
-    <!-- This is the Form -->
-    <form class="row" action="{{ route('settings', $settings['id']) }}" method="post">
-        @csrf
-        <div class="col-lg-6">
-            <div class="my-4">
-                <p><b>Payment Methods </b></p>
-                <select name="payment_methods" class="form-control">
-                    @foreach ($settings['method'] as $settingMethods)
-                    <option value="{{ $settingMethods['id'] }}" 
-                    @if ($settings['payment_methods']['id'] === $settingMethods['id']) 
-                     selected 
-                    @endif>{{ $settingMethods['method'] }}</option>
-                    @endforeach
-                </select>
+    <div class="col-lg-12">
+        <h2 class="my-4">Settings</h2>
+        <hr>
+        <!-- This is the Form -->
+        <form class="row" action="{{ route('settings', $settings['id']) }}" method="post">
+            @csrf
+            <div class="col-lg-6">
+                <div class="my-4">
+                    <p><b>Payment Methods </b></p>
+                    <select name="payment_methods" class="form-control">
+                        @foreach ($settings['method'] as $settingMethods)
+                            <option value="{{ $settingMethods['id'] }}" @if ($settings['payment_methods']['id'] === $settingMethods['id']) selected @endif>
+                                {{ $settingMethods['method'] }}</option>
+                        @endforeach
+                    </select>
+                </div>
+                <div class="my-4">
+                    <p><b>Payment Status </b></p>
+                    <select name="payment_status" class="form-control">
+                        @foreach ($settings['status'] as $settingStatus)
+                            <option value="{{ $settingStatus['id'] }}" @if ($settings['payment_status']['id'] === $settingStatus['id']) selected @endif>
+                                {{ $settingStatus['status'] }}</option>
+                        @endforeach
+                    </select>
+                </div>
+
+                <div class="my-4">
+                    <p><b>Multiplier default</b></p>
+                    <input name="multiplier_default" class="form-control" value="{{ $settings['multiplier_default'] }}">
+                </div>
+
+                <div class="my-4">
+                    <p><b>Multiplier Cost</b></p>
+                    <input name="multiplier_cost" class="form-control" placeholder="Your Multiplier cost is.."
+                        value="{{ $settings['multiplier_cost'] }}">
+                </div>
+
+                <div class="my-4">
+                    <!-- It shows the value of the card/inventory not the currency -->
+                    <h5><b>Currency</b></h5>
+                    <p>TCG Low</p>
+                    <select name="tcg_low" class="form-control">
+                        @foreach ($settings['currency_option'] as $settingCurrency)
+                            <option value="{{ $settingCurrency['id'] }}" @if ($settings['tcg_low'] === $settingCurrency['id']) selected @endif>
+                                {{ $settingCurrency['symbol'] }}</option>
+                        @endforeach
+                    </select>
+
+                    <p>TCG Mid</p>
+                    <select name="tcg_mid" class="form-control">
+                        @foreach ($settings['currency_option'] as $settingCurrency)
+                            <option value="{{ $settingCurrency['id'] }}" @if ($settings['tcg_mid'] === $settingCurrency['id']) selected @endif>
+                                {{ $settingCurrency['symbol'] }}</option>
+                        @endforeach
+                    </select>
+
+                    <p>TCG High</p>
+                    <select name="tcg_high" class="form-control">
+                        @foreach ($settings['currency_option'] as $settingCurrency)
+                            <option value="{{ $settingCurrency['id'] }}" @if ($settings['tcg_high'] === $settingCurrency['id']) selected @endif>
+                                {{ $settingCurrency['symbol'] }}</option>
+                        @endforeach
+                    </select>
+
+                </div>
+                <div class="my-4">
+                    <p><b>Sold Price</b></p>
+                    <select name="sold_price" class="form-control">
+                        @foreach ($settings['currency_option'] as $settingCurrency)
+                            <option value="{{ $settingCurrency['id'] }}" @if ($settings['sold_price'] === $settingCurrency['id']) selected @endif>
+                                {{ $settingCurrency['symbol'] }}</option>
+                        @endforeach
+                    </select>
+                </div>
+                <div class="my-4">
+                    <p><b>Estimated Card Cost</b></p>
+                    <select name="estimated_card_cost" class="form-control">
+                        @foreach ($settings['currency_option'] as $settingCurrency)
+                            <option value="{{ $settingCurrency['id'] }}" @if ($settings['estimated_card_cost'] === $settingCurrency['id']) selected @endif>
+                                {{ $settingCurrency['symbol'] }}</option>
+                        @endforeach
+                    </select>
+                </div>
+                <div class="my-4">
+                    <p><b>Shipment Cost</b></p>
+                    <select name="ship_cost" class="form-control">
+                        @foreach ($settings['currency_option'] as $settingCurrency)
+                            <option value="{{ $settingCurrency['id'] }}" @if ($settings['ship_cost'] === $settingCurrency['id']) selected @endif>
+                                {{ $settingCurrency['symbol'] }}</option>
+                        @endforeach
+                    </select>
+                </div>
+                <div class="my-4">
+                    <p><b>Shipment Price</b></p>
+                    <select name="ship_price" class="form-control">
+                        @foreach ($settings['currency_option'] as $settingCurrency)
+                            <option value="{{ $settingCurrency['id'] }}" @if ($settings['ship_price'] === $settingCurrency['id']) selected @endif>
+                                {{ $settingCurrency['symbol'] }}</option>
+                        @endforeach
+                    </select>
+                </div>
+
+                <button class="btn btn-primary" type="submit">Save Changes</button>
+
+
+
+
             </div>
-            <div class="my-4">
-                <p><b>Payment Status </b></p>
-                <select name="payment_status" class="form-control">
-                    @foreach ($settings['status'] as $settingStatus)
-                    <option value="{{ $settingStatus['id'] }}" @if ($settings['payment_status']['id']===$settingStatus['id']) selected @endif>{{ $settingStatus['status'] }}</option>
-                    @endforeach
-                </select>
-            </div>
-
-            <div class="my-4">
-                <p><b>Multiplier default</b></p>
-                <input name="multiplier_default" class="form-control" value="{{ $settings['multiplier_default'] }}">
-            </div>
-
-            <div class="my-4">
-                <p><b>Multiplier Cost</b></p>
-                <input name="multiplier_cost" class="form-control" placeholder="Your Multiplier cost is.." value="{{ $settings['multiplier_cost'] }}">
-            </div>
-
-            <div class="my-4">
-                <!-- It shows the value of the card/inventory not the currency -->
-                <h5><b>Currency</b></h5>
-                <p>TCG Low</p>
-                <select name="tcg_low" class="form-control">
-                    @foreach ($settings['currency_option'] as $settingCurrency)
-                    <option value="{{ $settingCurrency['id'] }}" @if ($settings['tcg_low']===$settingCurrency['id']) selected @endif>{{ $settingCurrency['symbol'] }}</option>
-                    @endforeach
-                </select>
-
-                <p>TCG Mid</p>
-                <select name="tcg_mid" class="form-control">
-                    @foreach ($settings['currency_option'] as $settingCurrency)
-                    <option value="{{ $settingCurrency['id'] }}" @if ($settings['tcg_mid']===$settingCurrency['id']) selected @endif>{{ $settingCurrency['symbol'] }}</option>
-                    @endforeach
-                </select>
-
-                <p>TCG High</p>
-                <select name="tcg_high" class="form-control">
-                    @foreach ($settings['currency_option'] as $settingCurrency)
-                    <option value="{{ $settingCurrency['id'] }}" @if ($settings['tcg_high']===$settingCurrency['id']) selected @endif>{{ $settingCurrency['symbol'] }}</option>
-                    @endforeach
-                </select>
-
-            </div>
-            <div class="my-4">
-                <p><b>Sold Price</b></p>
-                <select name="sold_price" class="form-control">
-                    @foreach ($settings['currency_option'] as $settingCurrency)
-                    <option value="{{ $settingCurrency['id'] }}" @if ($settings['sold_price']===$settingCurrency['id']) selected @endif>{{ $settingCurrency['symbol'] }}</option>
-                    @endforeach
-                </select>
-            </div>
-            <div class="my-4">
-                <p><b>Estimated Card Cost</b></p>
-                <select name="estimated_card_cost" class="form-control">
-                    @foreach ($settings['currency_option'] as $settingCurrency)
-                    <option value="{{ $settingCurrency['id'] }}" @if ($settings['estimated_card_cost']===$settingCurrency['id']) selected @endif>{{ $settingCurrency['symbol'] }}</option>
-                    @endforeach
-                </select>
-            </div>
-            <div class="my-4">
-                <p><b>Shipment Cost</b></p>
-                <select name="ship_cost" class="form-control">
-                    @foreach ($settings['currency_option'] as $settingCurrency)
-                    <option value="{{ $settingCurrency['id'] }}" @if ($settings['ship_cost']===$settingCurrency['id']) selected @endif>{{ $settingCurrency['symbol'] }}</option>
-                    @endforeach
-                </select>
-            </div>
-            <div class="my-4">
-                <p><b>Shipment Price</b></p>
-                <select name="ship_price" class="form-control">
-                    @foreach ($settings['currency_option'] as $settingCurrency)
-                    <option value="{{ $settingCurrency['id'] }}" @if ($settings['ship_price']===$settingCurrency['id']) selected @endif>{{ $settingCurrency['symbol'] }}</option>
-                    @endforeach
-                </select>
-            </div>
-
-            <button class="btn btn-primary" type="submit">Save Changes</button>
 
 
+        </form>
 
 
-        </div>
-
-
-    </form>
-
-
-</div>
-</div>
+    </div>
+    </div>
 @endsection
