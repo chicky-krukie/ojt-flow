b66366be8586a7d1d60de6a458461d4dd529f08e
JKRE3112 <jayeusebio31@gmail.com>
2023-04-20 16:06:53 -0700

    navbar

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -6,6 +6,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css"
         integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"
+        integrity="sha512-1JBNehX9mHyYvH8DRmBwOwxxDoM0F33mTQ2O2OxJRYmID9XOz1Yq3NqNwU0vm6teJen6REbN6Ud4a+z4J4LXrQ=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+
 
     <!-- Popup -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" />
@@ -17,22 +21,41 @@
     <script src="https://cdn.datatables.net/1.13.4/js/jquery.dataTables.min.js"></script>
     <link rel="stylesheet" href="https://cdn.datatables.net/1.13.4/css/jquery.dataTables.min.css">
     <link rel="stylesheet" href="https://cdn.datatables.net/buttons/2.3.6/css/buttons.dataTables.min.css">
-    <link rel="stylesheet" href="{{ url('css/custom.css') }}">
+
 
     <title>OJT FLOW</title>
 </head>
+<style>
+    body {
+        font-family: 'Nunito', sans-serif;
+    }
+
+    .thumbnail {
+        height: 14vh;
+        width: 7vw;
+    }
+</style>
 
 <body>
     <nav class="navbar navbar-expand-lg navbar-light bg-light col-lg-12">
         <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-            <a class="nav-item nav-link active" href="{{ route('home') }}">Home</a>
             <div class="navbar-nav ml-auto">
-                <a class="nav-item nav-link active" href="{{ url('inventory') }}">Inventory</a>
-                <a class="nav-item nav-link" href="{{ route('orders') }}">Orders</a>
-                <a class="nav-item nav-link" href="{{ route('settings') }}">Settings</a>
+                <a class="nav-item nav-link mx-2" href="{{ route('home') }}">Activity Logs
+                    <i class="fa fa-history" aria-hidden="true"></i>
+                </a>
+                <a class="nav-item nav-link mx-2" href="{{ url('inventory') }}">Inventory
+                    <i class="fa fa-archive" aria-hidden="true"></i>
+                </a>
+                <a class="nav-item nav-link mx-2" href="{{ route('orders') }}">Orders
+                    <i class="fa fa-shopping-cart" aria-hidden="true"></i>
+                </a>
+                <a class="nav-item nav-link mx-2" href="{{ route('settings') }}">Settings
+                    <i class="fa fa-cog" aria-hidden="true"></i>
+                </a>
             </div>
         </div>
     </nav>
+
 
     <div>
         @yield('content')
